7cdbf23e098e64f40be688c1b4e0c3732fece905
Jeff Diaku <jeff.fdiaku@gmail.com>
2024-12-02 13:35:08 -0600

    modify links

--- JS/auth.js
+++ JS/auth.js
@@ -12,6 +12,7 @@ document.getElementById('login-form').addEventListener('submit', (event) => {
   const email = document.getElementById('email').value.trim();
   const password = document.getElementById('password').value.trim();
 
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   // Message container
   const message = document.getElementById('message');
   message.innerHTML = '';
@@ -20,9 +21,12 @@ document.getElementById('login-form').addEventListener('submit', (event) => {
   if (email === validCredentials.email && password === validCredentials.password) {
     message.innerHTML = '<div class="success">Login succesfull.</div>'
     setTimeout(() => {
-      window.location.href = '/SBA-307-308/pages/newdash.html'; // Replace with your desired page
+      window.location.href = '/SBA-307-308/pages/newdash.html'; 
   }, 500);
-  }else if(email === "" || password === ""){
+  }else if(email !== "" && email !== validCredentials.email && !regex.test(email)){
+    message.innerHTML = '<div class="error">Enter a valid email adrress</div>'
+  }
+  else if(email === "" || password === ""){
      message.innerHTML = '<div class="error">Email or Password can not be empty.</div>'
   } else {
       message.innerHTML = '<div class="error">Invalid email or password.</div>';
