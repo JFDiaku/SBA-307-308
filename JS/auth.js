eb025328bd8b949547c2669367b10e6d7c9fe42d
Jeff Diaku <jeff.fdiaku@gmail.com>
2024-12-01 23:56:22 -0600

    first commit

---
+++ JS/auth.js
@@ -0,0 +1,34 @@
+// Hardcoded credentials
+const validCredentials = {
+  email: 'admin@gmail.com',
+  password: 'password123'
+};
+
+// Handle form submission
+document.getElementById('login-form').addEventListener('submit', (event) => {
+  event.preventDefault(); // Prevent page reload
+
+  // Get input values
+  const email = document.getElementById('email').value.trim();
+  const password = document.getElementById('password').value.trim();
+
+  // Message container
+  const message = document.getElementById('message');
+  message.innerHTML = '';
+
+  // Pseudo-authentication logic
+  if (email === validCredentials.email && password === validCredentials.password) {
+    message.innerHTML = '<div class="success">Login succesfull.</div>'
+    setTimeout(() => {
+      window.location.href = '../pages/newdash.html'; // Replace with your desired page
+  }, 500);
+  }else if(email === "" || password === ""){
+     message.innerHTML = '<div class="error">Email or Password can not be empty.</div>'
+  } else {
+      message.innerHTML = '<div class="error">Invalid email or password.</div>';
+  }
+
+  // Clear inputs (optional)
+  document.getElementById('username').value = '';
+  document.getElementById('password').value = '';
+});
---
+++ JS/fetchUsers.js
@@ -0,0 +1,36 @@
+
+fetch('/js/users.json')
+    .then(response => {
+        if (!response.ok) {
+            console.log("no response");
+        }
+        return response.json();
+    })
+    .then(users => {
+      
+
+        const userList = document.getElementById('user-list');
+
+        
+        users.forEach(user => {
+            const userCard = `
+            <div class="user-card">
+              <img class="user-image" src="${user.image}" alt="${user.firstname}'s image">
+              <div class="details">
+                <h3>${user.firstname} ${user.lastname}</h3>
+                <p>${user.sports.join(', ')}</p>
+              </div>
+            <div>
+              <p class="location">${user.city}, ${user.state}</p>
+              <a href=""><img src="../images/chat.png" class="chat-btn" alt="${user.firstname}'s image"> </a>
+            </div>
+              
+            </div>
+          `;
+            userList.innerHTML += userCard; 
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching users:', error);
+        document.getElementById('user-list').innerHTML = '<p>Error loading user data.</p>';
+    });
